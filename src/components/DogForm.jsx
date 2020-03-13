47c9371e29949fa64da3ec47ae1a81e0636db8bf
Nico <nicolascarrizo6@gmail.com>
2020-03-12 21:15:39 -0300

    [#171629396] Vista de ingreso de nuevos perros

--- src/components/DogForm.jsx
+++ src/components/DogForm.jsx
@@ -1,13 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./Styles.css";
 
 class DogForm extends Component {
-    render() {
-        return (
-            <div>
+  state = {
+    name: "",
+    photo: "",
+    ownarName: "",
+    ownerId: "",
+    contactMedium: [
+      {
+        type: "",
+        value: ""
+      }
+    ]
+  };
 
-            </div>
-        );
-    }
+  componentDidMount() {}
+
+  render() {
+    return (
+      <React.Fragment>
+        <header className="header">Header</header>
+
+        <div className="div reponsive" id="form">
+          <label className="titulo">Formulario de Ingreso de Perros</label>
+          <br />
+          <label className="campos" id="name">
+            Nombre del perro:
+          </label>{" "}
+          <input type="text"></input>
+          <br />
+          <label className="campos" id="photo">
+            Foto del perro:
+          </label>{" "}
+          <input classname="" type="file"></input>
+          <br />
+          <label className="campos" id="ownerName">
+            Nombre del dueño:
+          </label>{" "}
+          <input type="text"></input>
+          <br />
+          <label className="campos" id="ownerId">
+            Dni del dueño:
+          </label>{" "}
+          <input type="text"></input>
+          <ul>
+            Medios de Contacto
+            <li>
+              <label className="campos">Tipo:</label>{" "}
+              <select classname="campos" id="type">
+                <option value="Instagram">Instagram</option>
+                <option value="Facebook">Facebook</option>
+                <option value="Whatsapp">Whatsapp</option>
+                <option value="Telefono">Telefono</option>
+              </select>
+              <label className="campos">Contacto:</label>{" "}
+              <input type="text" id="value"></input> <br />
+              <button></button>
+            </li>
+          </ul>
+          <button onClick={() => this.SendFormData()} className="boton">
+            Guardar
+          </button>
+        </div>
+
+        <footer className="footer">Footer</footer>
+      </React.Fragment>
+    );
+  }
+
+  SendFormData() {
+    return alert("b");
+    //const data = new FormData(document.getElementById('form'));
+    //fetch('httpapi/v1/dog')
+  }
 }
 
 export default DogForm;
--- src/components/DogInfo.jsx
+++ src/components/DogInfo.jsx
@@ -1,40 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./Styles.css";
 
 class DogInfo extends Component {
-    state = {
-        dogName: "",
-        ownerName: "",
-        contactMediums: [],
+  state = {
+    dogName: "",
+    ownerName: "",
+    contactMediums: []
+  };
 
-    };
+  componentDidMount() {
+    this.setState({
+      dogName: "Chocoperro",
+      ownerName: "Chocoperro Owner",
+      contactMediums: [
+        {
+          contactMediumName: "Facebook",
+          value: "www.facebook.com/ChocoperroOwner"
+        },
+        {
+          contactMediumName: "Instagram",
+          value: "www.instagram.com/ChocoperroOwner"
+        },
+        { contactMediumName: "Tel", value: "2614785498" }
+      ]
+    });
+  }
 
-    componentDidMount() {
-        this.setState(
-            {
-                    dogName: "Chocoperro",
-                    ownerName: "Chocoperro Owner",
-                    contactMediums: [   {contactMediumName: "Facebook", value: "www.facebook.com/ChocoperroOwner"},
-                                        {contactMediumName: "Instagram", value: "www.instagram.com/ChocoperroOwner"},
-                                        {contactMediumName: "Tel", value: "2614785498"}
-                    ]
-            }
-            );
+  render() {
+    return (
+      <React.Fragment>
+        <header className="header">Header</header>
 
-    }
-
-    render() {
-        return (
-            <div>
-                <img src={""} alt={"Perro image"}/><br/>
-                <label>Nombre: {this.state.dogName}</label><br />
-                <label>Dueño: {this.state.ownerName}</label>
-                <ul>
-                {this.state.contactMediums.map((contactMedium) => <li>{contactMedium.contactMediumName} : {contactMedium.value}</li>)}
-                </ul>
-            </div>
-        );
-    }
+        <div className="div responsive">
+          <img src={""} alt={"Perro image"} />
+          <br />
+          <label>Nombre: {this.state.dogName}</label>
+          <br />
+          <label>Dueño: {this.state.ownerName}</label>
+          <ul>
+            {this.state.contactMediums.map(contactMedium => (
+              <li>
+                {contactMedium.contactMediumName} : {contactMedium.value}
+              </li>
+            ))}
+          </ul>
+          <a href="https://www.instagram.com">
+          <span class="instagram bottomleft">
+            <span class="fa fa-instagram"></span>
+          </span>
+          </a>
+        </div>
+        <footer className="footer">Footer</footer>
+      </React.Fragment>
+    );
+  }
 }
 
 export default DogInfo;
-
